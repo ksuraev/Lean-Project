652e34b722f76fa2e92ff8dfeec6d5606cfa64a1
Kate Suraev <ksuraev@outlook.com>
2025-11-19 18:54:11 +1100

    Restructure bijection proof into two namespaces

--- LeanProject/EquivalenceRelations/Bijection.lean
+++ LeanProject/EquivalenceRelations/Bijection.lean
@@ -7,6 +7,135 @@ import LeanProject.Partitions.Properties
 -- Bijection between Equivalence Relations and Partitions
 
 namespace EqRelBijection
+
+open EqClasses_Setoid
+open InducedRelation
+open Partitions
+
+-- Rewrite subset_equal to use in the bijection proof
+lemma eq_of_mem
+  {α} (P : Partition α) {s t : Set α}
+  (hs : s ∈ P.subsets) (ht : t ∈ P.subsets)
+  {a : α} (ha_s : a ∈ s) (ha_t : a ∈ t) :
+  s = t := by
+  apply subsets_equal P s t hs ht a ha_s ha_t
+
+-- Define F and G functions and prove that the composite maps are identity functions
+-- https://github.com/kbuzzard/xena/blob/268b3bab45ba8fbed09b45cbbdc80a3813f73b5e/partitions.lean#L4
+
+-- Helper lemma to show that a subset in a partition is equal to the equivalence class induced by the partition
+lemma subset_eq_equiv_class {X : Type _} (P : Partition X) (s : Set X) (hs : s ∈ P.subsets)
+  (a : X) (ha : a ∈ s) :
+  s = {x | induced_rel P x a} := by
+  ext x
+  constructor
+  · intro hxs
+    rw[Set.mem_setOf_eq]
+    use s
+  · intro hPxa
+    rcases hPxa with ⟨t, htP, hxt, hat⟩
+    have hst : s = t := eq_of_mem P hs htP ha hat
+    rw[hst]
+    exact hxt
+
+-- Define the functions F and G
+def F (S : Setoid X) : Partition X :=
+  eq_classes_form_partition_sub S
+
+
+def G (P : Partition X) : Setoid X :=
+  ⟨induced_rel P, induced_rel_is_equivalence P⟩
+
+
+-- Almost the same as the right_inv and left_inv proofs above
+theorem FG_eq_id (P : Partition X) : F (G P) = P := by
+  ext s -- s ∈ (F (G P)).subsets ↔ s ∈ P.subsets
+  dsimp [F, G, eq_classes_form_partition_sub]
+  constructor
+  -- 1. Assume s ∈ eq_classes { r := induced_rel P, iseqv := ⋯ }
+  · intro hs
+    -- there exists a ∈ X such that s = {x | induced_rel P x a}
+    rcases hs with ⟨a, rfl⟩
+    -- Simplify the goal to show {x | induced_rel P x a} ∈ P.subsets
+    simp [equiv_class]
+    -- show that a ∈ ⋃₀ P.subsets
+    have ha : a ∈ Set.sUnion P.subsets := by
+      rw [P.union_eq_univ]
+      exact Set.mem_univ a
+    -- there exists t ∈ P.subsets such that a ∈ t
+    rcases Set.mem_sUnion.mp ha with ⟨t, htP, hat⟩
+    -- show {x | induced_rel P x a} = t
+    have h_eq : { x | induced_rel P x a } = t := by
+      apply symm (subset_eq_equiv_class P t htP a hat)
+    -- Apply the equality to rewrite the goal
+    rw [h_eq] -- t ∈ P.subsets
+    exact htP
+  -- 2. Assume s ∈ P.subsets
+  · intro hsP
+    -- there exists a ∈ X such that a ∈ s
+    rcases P.nonempty_subsets s hsP with ⟨a, ha⟩ -- ha : a ∈ s
+    use a -- s = [a]_{ r := induced_rel P, iseqv := ⋯ }
+    simp [equiv_class] -- ⊢ s = {x | induced_rel P x a}
+    ext x -- apply extensionality to x ∈ s ↔ x ∈ {x | induced_rel P x a}
+    constructor
+    · -- Assume x ∈ s
+      intro hxs
+      rw [Set.mem_setOf_eq]
+      use s
+    · -- Assume x ∈ {x | induced_rel P x a}
+      intro hRxa
+      -- there exists t ∈ P.subsets such that x ∈ t and a ∈ t
+      rcases hRxa with ⟨t, htP, hxt, hat⟩
+      -- show s = t
+      have hst : s = t := eq_of_mem P hsP htP ha hat
+      rw [hst] -- Goal: x ∈ t
+      exact hxt
+
+
+theorem GF_eq_id (S : Setoid X) : G (F S) = S := by
+  ext x y -- (G (F S)) x y ↔ S x y
+  dsimp [G, F, induced_rel, eq_classes_form_partition_sub] -- simplify definitions
+  constructor
+  · -- 1. Assume (∃ s ∈ eq_classes S, x ∈ s ∧ y ∈ s)
+    intro hxy
+    -- Add partition definition
+    let P := (eq_classes_form_partition_sub S : Partition X)
+    -- Then, there exists s ∈ eq_classes S such that x ∈ s and y ∈ s
+    rcases hxy with ⟨s, hsP, ⟨hx, hy⟩⟩
+    -- Introduce equality of subsets
+    have hP_eq : eq_classes S = P.subsets := (eq_classes_form_partition_sub S).property
+    -- Rewrite hsP: s ∈ eq_classes S to s ∈ P.subsets
+    rw [hP_eq] at hsP
+    -- There exists a ∈ X such that x ∈ [a]_R and y ∈ [a]_R
+    rcases hsP with ⟨a, rfl⟩
+    -- Therefore, x ≈ y under S
+    exact S.trans hx (S.symm hy)
+  · -- 2. Assume S x y
+    intro hRxy
+    -- Use the equivalence class of x under S
+    use equiv_class S x
+    constructor
+    · -- Show [x]_S ∈ eq_classes S
+      exact ⟨x, rfl⟩
+    · -- Show x ∈ [x]_S and y ∈ [x]_S
+      constructor
+      · -- x ∈ [x]_S by reflexivity
+        exact S.refl x
+      · -- y ∈ [x]_S by symmetry and transitivity
+        exact S.symm hRxy
+
+-- Final bijection definition
+def partitions_biject_with_equivalence_relations :
+  Equiv (Setoid X) (Partition X) := by
+  refine
+    { toFun := F,
+      invFun := G,
+      left_inv := GF_eq_id,
+      right_inv := FG_eq_id }
+
+end EqRelBijection
+
+namespace EqRelBijectionDraft
 open EqClasses_Setoid
 open InducedRelation
 open Partitions
@@ -16,7 +145,6 @@ open Partitions
 -- Using the equiv structure to define a bijection between equivalence relations and partitions
 -- Source: https://github.com/leanprover-community/mathlib4/blob/82ce028190bb8ed1a85ff2347d892c72aa439bef/Mathlib/Logic/Equiv/Defs.lean#L62-L73
 
--- Rewrite subset_equal to use in the bijection proof
 lemma eq_of_mem
   {α} (P : Partition α) {s t : Set α}
   (hs : s ∈ P.subsets) (ht : t ∈ P.subsets)
@@ -105,99 +233,4 @@ def eqrel_partition_bijection (X : Type _) : { R : X → X → Prop // Equivalen
         exact hxt
 }
 
--- Alternate approach - define F and G functions and prove that the composite maps are identity functions
--- https://github.com/kbuzzard/xena/blob/268b3bab45ba8fbed09b45cbbdc80a3813f73b5e/partitions.lean#L4
-
--- Helper lemma to show that a subset in a partition is equal to the equivalence class induced by the partition
-lemma subset_eq_equiv_class {X : Type _} (P : Partition X) (s : Set X) (hs : s ∈ P.subsets)
-  (a : X) (ha : a ∈ s) :
-  s = {x | induced_rel P x a} := by
-  ext x
-  constructor
-  · intro hxs
-    rw[Set.mem_setOf_eq]
-    use s
-  · intro hPxa
-    rcases hPxa with ⟨t, htP, hxt, hat⟩
-    have hst : s = t := eq_of_mem P hs htP ha hat
-    rw[hst]
-    exact hxt
-
--- Define the functions F and G
-def F (S : Setoid X) : Partition X :=
-  eq_classes_form_partition_sub S
-
-
-def G (P : Partition X) : Setoid X :=
-  ⟨induced_rel P, induced_rel_is_equivalence P⟩
-
-
--- Almost the same as the right_inv and left_inv proofs above
-theorem FG_eq_id (P : Partition X) : F (G P) = P := by
-  ext s
-  dsimp [F, G, eq_classes_form_partition_sub]
-  constructor
-  · -- ⊢ s ∈ eq_classes { r := induced_rel P, iseqv := ⋯ } → s ∈ P.subsets
-    intro hs
-    rcases hs with ⟨a, rfl⟩
-    simp [equiv_class]
-    have ha : a ∈ Set.sUnion P.subsets := by
-      rw [P.union_eq_univ]
-      exact Set.mem_univ a
-    rcases Set.mem_sUnion.mp ha with ⟨t, htP, hat⟩
-    have h_eq : { x | induced_rel P x a } = t := by
-      apply symm (subset_eq_equiv_class P t htP a hat)
-    rw [h_eq]
-    exact htP
-  · -- ⊢ s ∈ P.subsets → s ∈ eq_classes { r := induced_rel P, iseqv := ⋯ }
-    intro hsP
-    rcases P.nonempty_subsets s hsP with ⟨a, ha⟩
-    use a
-    simp [equiv_class] -- ⊢ = {x | induced_rel P x a}
-    ext x -- ⊢ x ∈ s ↔ x ∈ {x | induced_rel P x a}
-    constructor
-    · -- ⊢ x ∈ s → x ∈ {x | induced_rel P x a}
-      intro hxs
-      rw [Set.mem_setOf_eq]
-      use s
-    · -- ⊢ x ∈ {x | induced_rel P x a} → x ∈ s
-      intro hRxa
-      rcases hRxa with ⟨t, htP, hxt, hat⟩
-      have hst : s = t := eq_of_mem P hsP htP ha hat
-      rw [hst]
-      exact hxt
-
-
-theorem GF_eq_id (S : Setoid X) : G (F S) = S := by
-  ext x y -- (G (F S)) x y ↔ S x y
-  dsimp [G, F, induced_rel, eq_classes_form_partition_sub] -- simplify definitions
-  constructor
-  · intro hxy -- ⊢ S x y
-    let P := (eq_classes_form_partition_sub S : Partition X)
-    rcases hxy with ⟨s, hsP, ⟨hx, hy⟩⟩ -- hsP : s ∈ eq_classes S, hx : x ∈ s, hy : y ∈ s
-    have hP_eq : eq_classes S = P.subsets := (eq_classes_form_partition_sub S).property
-    rw [hP_eq] at hsP -- s ∈ P.subsets
-    rcases hsP with ⟨a, rfl⟩ -- hx : x ∈ [a]_R, hy : y ∈ [a]_R
-    exact S.trans hx (S.symm hy) -- Show x ≈ y
-  · intro hRxy -- ⊢ ∃ s ∈ eq_classes S, x ∈ s ∧ y ∈ s
-    use equiv_class S x
-    constructor
-    · -- ⊢ [x]_S ∈ eq_classes S
-      exact ⟨x, rfl⟩
-    · -- ⊢ x ∈ [x]_S ∧ y ∈ [x]_S
-      constructor
-      · -- x ∈ [x]_S
-        exact S.refl x
-      · -- y ∈ [x]_S
-        exact S.symm hRxy
-
--- Final bijection theorem
-def partitions_biject_with_equivalence_relations :
-  Equiv (Setoid X) (Partition X) := by
-  refine
-    { toFun := F,
-      invFun := G,
-      left_inv := GF_eq_id,
-      right_inv := FG_eq_id }
-
-end EqRelBijection
+end EqRelBijectionDraft
