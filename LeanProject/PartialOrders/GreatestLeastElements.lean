e2b883d87f67fbbd3b0a19662bc127090b5b223c
Kate Suraev <ksuraev@outlook.com>
2025-12-04 11:53:34 +1100

    Add definitions and theorem for greatest and least elements in PartialOrders

---
+++ LeanProject/PartialOrders/GreatestLeastElements.lean
@@ -0,0 +1,22 @@
+import Mathlib.Order.Defs.PartialOrder
+import Mathlib.Data.Fintype.Defs
+
+namespace Greatest
+
+-- Define greatest element 'x'
+def greatest {S : Type _} [PartialOrder S] (x : S) : Prop := ∀ {y : S}, y ≤ x
+
+-- Show uniqueness of greatest element
+theorem greatest_is_unique (α : Type _) [Fintype α] [PartialOrder α] [Nonempty α] [DecidableRel (fun (x y : α) => x ≤ y)] [DecidableEq α] : ∀ x z : α, greatest x ∧ greatest z → x = z := by
+  intro x z hxz
+  -- Assume greatest x and greatest z
+  rcases hxz with ⟨hx, hz⟩
+  -- Use antisymm prop to show x = z
+  exact le_antisymm hz hx
+
+end Greatest
+
+namespace Least
+-- Define least element 'x'
+def least {S : Type _} [PartialOrder S] (x : S) : Prop := ∀ {y : S}, x ≤ y
+end Least
