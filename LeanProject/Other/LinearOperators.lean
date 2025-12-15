5fbad16e2552ebb3cd760b4126a8711d95a4d33d
Kate Suraev <ksuraev@outlook.com>
2025-12-15 22:46:38 +1100

    Refactor LinearOperator structure to use NormedAddCommGroup and InnerProductSpace; add orthogonal complement definition

--- LeanProject/Other/LinearOperators.lean
+++ LeanProject/Other/LinearOperators.lean
@@ -2,14 +2,16 @@ import Mathlib.LinearAlgebra.FiniteDimensional.Defs
 import Mathlib.Algebra.Field.Basic
 import Mathlib.Algebra.Module.Basic
 import Mathlib.Data.Real.Basic
+import Mathlib.Analysis.InnerProductSpace.Defs
+import Mathlib.Analysis.InnerProductSpace.Basic
 
 namespace LinearOperators
 
--- V and W - vector spaces over R
--- Module is technically vector space in Lean
-variable {V W : Type _} [AddCommGroup V] [Module ℝ V] [AddCommGroup W] [Module ℝ W]
+variable {V W : Type _} [NormedAddCommGroup V] [InnerProductSpace ℝ V] [NormedAddCommGroup W] [InnerProductSpace ℝ W]
 
-structure LinearOperator (V W : Type _) [AddCommGroup V] [Module ℝ V] [AddCommGroup W] [Module ℝ W] where
+structure LinearOperator (V W : Type _)
+  [NormedAddCommGroup V] [InnerProductSpace ℝ V]
+  [NormedAddCommGroup W] [InnerProductSpace ℝ W] where
   toFun : V → W
   map_add' : ∀ (u v : V), toFun (u + v) = toFun u + toFun v
   map_smul' : ∀ (k : ℝ) (u : V), toFun (k • u) = k • toFun u
@@ -17,7 +19,8 @@ structure LinearOperator (V W : Type _) [AddCommGroup V] [Module ℝ V] [AddComm
 -- Allows for shorter hand notation of T.toFun v as T v
 instance : CoeFun (LinearOperator V W) (fun _ => V → W) := ⟨LinearOperator.toFun⟩
 
-def nullspace (T : LinearOperator V W) : Set V := {v : V | T v = 0}
+def nullspace (T : LinearOperator V W) : Set V :=
+  {v : V | T v = 0}
 
 def range (T : LinearOperator V W) : Set W := { w : W | ∃ v, T v = w }
 
@@ -61,3 +64,36 @@ def range_is_subspace (T : LinearOperator V W) : Subspace ℝ W where
     rw[T.map_smul']
 
 end LinearOperators
+
+namespace OrthogonalComplements
+open LinearOperators
+
+variable {V W : Type _} [NormedAddCommGroup V] [InnerProductSpace ℝ V] [NormedAddCommGroup W] [InnerProductSpace ℝ W]
+
+def orthogonal_complement
+(S : Subspace ℝ V) : Subspace ℝ V where
+  -- All x such that ⟨x, s⟩ = 0 for every s in S
+  carrier := { x : V | ∀ s ∈ S, inner ℝ s x = 0 }
+  zero_mem' := by
+    intro s hs
+    rw [inner_zero_right]
+  add_mem' := by
+    intro a b ha hb
+    rw[Set.mem_setOf_eq] at *
+    intro s hs
+    rw[inner_add_right]
+    rw[ha, hb, zero_add]
+    exact hs
+    exact hs
+  smul_mem' := by
+    intro c x hx
+    rw[Set.mem_setOf_eq] at *
+    intro s hs
+    rw[inner_smul_right_eq_smul, hx, smul_zero]
+    exact hs
+
+
+
+
+
+end OrthogonalComplements
